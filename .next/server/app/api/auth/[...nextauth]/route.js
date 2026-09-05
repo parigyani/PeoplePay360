220a5e286bfd0cada304541b7c337eb3f00986d3
parigyan6828 <220368753+parigyani@users.noreply.github.com>
2026-09-05 17:49:30 +0530

    chore: stop tracking .next build output

--- .next/server/app/api/auth/[...nextauth]/route.js
+++
@@ -1,6 +0,0 @@
-var R=require("../../../../chunks/[turbopack]_runtime.js")("server/app/api/auth/[...nextauth]/route.js")
-R.c("server/chunks/[root-of-the-server]__05lk9k5._.js")
-R.c("server/chunks/[root-of-the-server]__1zy-j4m._.js")
-R.c("server/chunks/_next-internal_server_app_api_auth_[___nextauth]_route_actions_08nexdk.js")
-R.m(59061)
-module.exports=R.m(59061).exports
